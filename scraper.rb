8bb645f734f7fe6f0726d16bf3a0fd3124889bf0
Chris Mytton <chrismytton@gmail.com>
2015-10-23 12:29:41 +0100

    Initial scraper

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,13 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'wikidata/fetcher'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+pages = ['Kategorija:Poslanci_6._državnega_zbora_Republike_Slovenije']
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+pages = pages.map { |c| WikiData::Category.new(c, 'sl').wikidata_ids }
+
+pages.flatten.uniq.each do |id|
+  data = WikiData::Fetcher.new(id: id).data('sl')
+  next if data.nil?
+  warn "#{data[:id]} #{data[:name]}"
+  ScraperWiki.save_sqlite([:id], data)
+end
