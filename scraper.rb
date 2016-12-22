31e2e0c6bacea0b11c1602bd6a96fab55bc54b6c
Tony Bowden <tony@tmtm.com>
2016-12-22 08:26:00 +0000

    Turn off the Rebuilder notification
    We do that with webhooks now

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,9 @@
 require 'wikidata/fetcher'
 
 from_cat = WikiData::Category.new( 'Kategorija:Poslanci_7._državnega_zbora_Republike_Slovenije', 'sl').member_titles
-from_page = EveryPolitician::Wikidata.wikipedia_xpath( 
+from_page = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://sl.wikipedia.org/wiki/Seznam_poslancev_7._državnega_zbora_Republike_Slovenije',
   xpath: '//h2/following-sibling::ul//a[contains(@href, "/wiki/") and not(@class="new")]/@title',
-) 
+)
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { sl: from_cat | from_page })
-warn EveryPolitician::Wikidata.notify_rebuilder
