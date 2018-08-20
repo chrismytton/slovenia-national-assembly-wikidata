25748352c10b016c51ffb9f98a88320b33e4062b
Tony Bowden <tony@tmtm.com>
2018-08-20 09:24:08 +0100

    Also get Term 8 members

--- scraper.rb
+++ scraper.rb
@@ -3,13 +3,20 @@
 
 require 'wikidata/fetcher'
 
-from_cat = WikiData::Category.new( 'Kategorija:Poslanci_7._državnega_zbora_Republike_Slovenije', 'sl').member_titles
+from_cat = WikiData::Category.new( 'Kategorija:Poslanci_7._državnega_zbora_Republike_Slovenije', 'sl').member_titles |
+           WikiData::Category.new( 'Kategorija:Poslanci_8._državnega_zbora_Republike_Slovenije', 'sl').member_titles
+
 from_page = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://sl.wikipedia.org/wiki/Seznam_poslancev_7._državnega_zbora_Republike_Slovenije',
   xpath: '//h2/following-sibling::ul//a[contains(@href, "/wiki/") and not(@class="new")]/@title',
 )
 
-sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39 [ ps:P39 wd:Q21296001  ; pq:P2937 wd:Q19932345 ] }'
+sparq = <<~SPARQL
+  SELECT DISTINCT ?item WHERE {
+    ?item p:P39 [ ps:P39 wd:Q21296001 ; pq:P2937 ?term ] .
+    ?term p:P31/pq:P1545 ?ordinal FILTER (xsd:integer(?ordinal) >= 7)
+  }
+SPARQL
 ids = EveryPolitician::Wikidata.sparql(sparq)
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { sl: from_cat | from_page })
